523d98a82b4db6528d5db91857f894d415d3d184
dazacher <dazacher2000@yahoo.com>
2021-07-15 08:27:57 -0700

    Used use and Ada.Integer_Tet_IO.

--- src/hello.adb
+++ src/hello.adb
@@ -1,7 +1,11 @@
-with Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO;
 
 procedure Hello is
+    X : integer := 10;
 
 begin
-   Ada.Text_IO.Put_Line("Hello Ada 2012!");
+    Put_Line("Hello Ada 2012!" & " You can concantonate with Ada!" & Integer'Image(X));
+    Put("Hello, Ada 2012.");
+    Ada.Integer_Text_IO.Put(X); New_Line;
 end Hello;
